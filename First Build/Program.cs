49a1d0ee24e63d02f42c4e2ad84602e3e22189d5
David Sills <David.sills1000@gmail.com>
2023-04-20 18:28:34 -0500

    Add project files.

---
+++ First Build/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Please type your name!");
+
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("---------------------------------------------------------");
+Console.WriteLine($"Hello {name}. Right now it is {date}. This is David's Amazing Math Game! So glad you are here to try!\n");
+Console.WriteLine($@"What game would youn like to play today? Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the Program");
+Console.WriteLine("---------------------------------------------");
+
+Console.ReadLine();
