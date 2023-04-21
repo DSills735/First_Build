dbe581786f7b24c1b1531b34ff655673e616bb66
David Sills <David.sills1000@gmail.com>
2023-04-20 19:32:25 -0500

    Basic menus in place. No other options past thtat.

--- First Build/Program.cs
+++ First Build/Program.cs
@@ -4,13 +4,58 @@ var name = Console.ReadLine();
 var date = DateTime.UtcNow;
 
 Console.WriteLine("---------------------------------------------------------");
-Console.WriteLine($"Hello {name}. Right now it is {date}. This is David's Amazing Math Game! So glad you are here to try!\n");
+Console.WriteLine($"Hello {name. ToUpper()}. Right now it is {date}. This is David's Amazing Math Game! So glad you are here to try!\n");
 Console.WriteLine($@"What game would youn like to play today? Choose from the options below:
 A - Addition
 S - Subtraction
 M - Multiplication
 D - Division
-Q - Quit the Program");
-Console.WriteLine("---------------------------------------------");
+Q - Exit to Desktop");
+Console.WriteLine("---------------------------------------------------------");
 
-Console.ReadLine();
+var gameSelected = Console.ReadLine();
+
+if (gameSelected.Trim().ToLower() == "a")
+{
+    AdditionGame("Addition Game Selected!");
+}
+else if (gameSelected.Trim().ToLower() == "s")
+{
+    SubtractionGame("Subtraction Game Selected!");
+}
+else if (gameSelected.Trim().ToLower() == "m")
+{
+    MultiplicationGame("Multiplication Game Selected!Q");
+}
+else if (gameSelected.Trim().ToLower() == "d")
+{
+    DivisionGame("Division Game Selected!");
+}
+else if (gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye :(");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Invalid Input. Please Try Again");
+}
+
+void AdditionGame(string message)
+{
+    Console.WriteLine(message);
+}
+    
+void SubtractionGame(string message)
+{
+    Console.WriteLine(message);
+}
+void MultiplicationGame(string message)
+{
+    Console.WriteLine(message);
+}
+void DivisionGame(string message)
+{
+    Console.WriteLine(message);
+}
+    
